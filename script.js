e793e311e0699a3d0dad03cf4252b3e290de154c
Ryell Martinez <ryell.martinez@student.tdsb.on.ca>
2024-10-23 11:59:59 -0400

    sa git commit -m sa git commit -m sa

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
-let sideA=2;
-let sideB=2;
-let sideC=2;
-function heronF(a,b,c){
+let sideAh=2;
+let sideBh=2;
+let sideCh=2;
+function heronf(a,b,c){
     return (1/4)*(Math.sqrt(4*Math.pow(a,2)*Math.pow(b,2)-(Math.pow((Math.pow(a,2)+Math.pow(b,2)-Math.pow(c,2)),2))))
 }
-let area = heronF(sideA,sideB,sideC);
-console.log(heronF(sideA,sideB,sideC));
+let area = heronf(sideAh,sideBh,sideCh);
+console.log(area);
+
+let sideAac=2;
+let sideBac=2;
+let angleAac=2;
+function ambf(a,b,angle){
+    angle = angle*180/Math.PI
+    console.log(angle);
+    let h = b*Math.sin(angle)
+    if(h<a<b){
+        return("There are two triangles")
+    }
+    return 
+}
+console.log(ambf(0.5,1,1));
